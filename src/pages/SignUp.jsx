f83f979206c14e9fa1c9109904f6b65d3b0af854
J-Viral <30624865+J-Viral@users.noreply.github.com>
2024-01-31 15:03:03 +0530

    Update SignUp.jsx

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -298,9 +298,6 @@ const SignUp = () => {
             [id]: type === "checkbox" ? checked : value,
         }));
 
-        if ((id === "fname" || id === "lname") && !/^[A-Za-z]+$/.test(value)) {
-            alert("Only alphabets are allowed."); }
-
         
     };
 
@@ -310,6 +307,13 @@ const SignUp = () => {
             if (formData.password !== formData.cpassword) {
                 throw new Error("Passwords do not match");
             }
+
+            const passwordRegex = /^(?=.[a-z])(?=.[A-Z])(?=.\d)(?=.[@$!%?&])[A-Za-z\d@$!%?&]{8,}$/;
+        if (!passwordRegex.test(formData.password)) {
+          throw new Error(
+            "Requires at least one lowercase letter,uppercase letter, special character, and number."
+          );
+        }
   
             const { data, error } = await supabase.auth.signUp({
                 email: formData.email,
